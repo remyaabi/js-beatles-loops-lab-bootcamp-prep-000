d2a8133e605d26b1fc18e2b0b0899bb8c24f4ba7
Remya Jacob <remyaabi@gmail.com>
2018-01-18 23:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
 const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
