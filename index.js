da493db66056f9a2abf8e7d40ab2f6d0de7226e8
Remya Jacob <remyaabi@gmail.com>
2018-01-18 22:30:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var musicians =['Zakir Hussain','Kadri Gopalnath','Dr. L. Subramaniam']
 var instruments=['tabla','saxophone','violin']
 function theBeatlesPlay(musicians,instruments){
-  var musiciansinstruments=[]
-  for(let i=0;i<(musicians.length-1 || instruments.length-1)
+  var musiciansinstruments=[];
+  for(let i=0;i<(musicians.length-1 || instruments.length-1);i++){
+    musiciansinstruments[i]=`${musicians[i]} plays ${instruments[i]}`;
+  }
+  return musiciansinstruments;
 }
