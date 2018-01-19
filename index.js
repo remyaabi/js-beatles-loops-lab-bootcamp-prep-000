5514adde55d420a19f8e288a3055d4d6c714f7fe
Remya Jacob <remyaabi@gmail.com>
2018-01-19 00:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
+
 function theBeatlesPlay(musicians,instruments){
   var musiciansinstruments=[];
-  for(let i=0;i<(musicians.length-1 || instruments.length-1);i++){
-    musiciansinstruments[i]=`${musicians[i]} plays ${instruments[i]}`;
+  for(let i=0;i< (musicians.length-1 ||        instruments.length-1);i++){
+    musiciansinstruments[i]=`${musicians[i]}       plays ${instruments[i]}`;
   }
   return musiciansinstruments;
 }
@@ -18,11 +21,12 @@ function johnLennonFacts(facts){
   var n=0;
   var modifiedfacts=[]
   while (n<facts.length){
-    modifiedfacts[n]=`${facts[n]}+!!!`;
+    modifiedfacts[n]=`${facts[n]}!!!`;
     n++;
   }
   return modifiedfacts;
 }
+
 var number=2;
 function iLoveTheBeatles(number){
   var counter=0;
